9051340eb46a0dec444eeffa0281a81ddeeabe6d
Sophia Zocoler¨git config --global user.name sophiazocoler@gmail.com¨ <sophiazocoler@gmail.com>
2023-05-24 00:38:13 -0300

    update router

--- src/routers/kindRouter.ts
+++ src/routers/kindRouter.ts
@@ -4,5 +4,6 @@ import * as kindController from '../controllers/kindController.js';
 
 const kindRouter = Router();
 kindRouter.get('/kinds', kindController.getKinds)
+kindRouter.post('/initialize-kinds', kindController.initializeKinds)
 
 export default kindRouter;
